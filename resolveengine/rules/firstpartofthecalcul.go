ab2011e26e6126850bd63b38cb9643ae6c1263aa
nleonard <nathan.leonard.contact@gmail.com>
2025-09-25 16:30:58 +0200

    Progression dans la gestion de la virgule en entrée.

--- resolveengine/rules/firstpartofthecalcul.go
+++ resolveengine/rules/firstpartofthecalcul.go
@@ -1,7 +1,10 @@
 package rules
 
 func FirstPart(sample1 []rune, caracter rune, symbol rune) []rune {
-	if (caracter >= '0' && caracter <= '9') && symbol == ' ' {
+	if (caracter >= '0' && caracter <= '9') || (caracter == '.') || (caracter == ',') && symbol == ' ' {
+		if caracter == ',' {
+			caracter = '.'
+		}
 		sample1 = append(sample1, caracter)
 	}
 	return sample1
--- resolveengine/rules/secondpartofthecalcul.go
+++ resolveengine/rules/secondpartofthecalcul.go
@@ -1,7 +1,10 @@
 package rules
 
 func SecondPart(sample2 []rune, caracter rune, symbol rune) []rune {
-	if (caracter >= '0' && caracter <= '9') && symbol != ' ' {
+	if (caracter >= '0' && caracter <= '9') || (caracter == '.') || (caracter == ',') && symbol != ' ' {
+		if caracter == ',' {
+			caracter = '.'
+		}
 		sample2 = append(sample2, caracter)
 	}
 	return sample2
